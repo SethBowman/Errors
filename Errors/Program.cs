d23b676f731afa1b727588917b083747afe64bc5
Seth Bowman <brandonsethbowman@gmail.com>
2023-08-09 13:57:34 -0500

    Add project files.

---
+++ Errors/Program.cs
@@ -0,0 +1,39 @@
+﻿using Errors;
+//Compile Time Errors
+
+//int number = "Seth";
+
+//Runtime Errors
+
+//Console.WriteLine("Enter a number: ");
+//var userInput = int.Parse(Console.ReadLine());
+//Console.WriteLine(userInput);
+
+//var numbers = new int[] { 1, 2, 3, 4 };
+
+//Console.WriteLine(numbers[10]);
+
+//Logical Errors
+
+//var name = "Bob";
+//var age = 29;
+
+//Console.WriteLine($"Hello, my name is {age} and I am {name} years old.");
+
+
+//Console.WriteLine(result);
+
+//for(int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+
+
+var a = 10;
+var b = 5;
+int result = MathHelper.Add(a, b);
+
+Console.WriteLine(result);
